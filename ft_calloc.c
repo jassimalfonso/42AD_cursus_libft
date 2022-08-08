476afd2d3296dbc5532fb50b7adfbe96e0ae5249
Jassim Alfonso <jassim.alfonso@gmail.com>
2022-08-08 11:14:06 +0400

    ft_calloc modified

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,21 @@
 /*   By: jalfonso <jalfonso@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 03:02:08 by jalfonso          #+#    #+#             */
-/*   Updated: 2022/07/16 03:13:09 by jalfonso         ###   ########.fr       */
+/*   Updated: 2022/08/08 11:10:01 by jalfonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
+	size_t	num;
 
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-		return (ptr);
-	ft_bzero(ptr, nmemb * size);
+	num = count * size;
+	ptr = (void *)malloc(num);
+	if (!ptr || count == SIZE_MAX || size == SIZE_MAX)
+		return (NULL);
+	ft_bzero(ptr,	num);
 	return (ptr);
 }
