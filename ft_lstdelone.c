bdc748024dd4992fe3b4085ec919e9532a90c859
Jassim Alfonso <jassim.alfonso@gmail.com>
2022-08-18 12:58:23 +0400

    ft_lstdelone added

---
+++ ft_lstdelone.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalfonso <jalfonso@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/18 12:25:07 by jalfonso          #+#    #+#             */
+/*   Updated: 2022/08/18 12:28:43 by jalfonso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+}
