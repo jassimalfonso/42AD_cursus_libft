8fcb8a1e6b7f36bc404261fdf57d5035565c4cef
Jassim Alfonso <jalfonso@lab1r3s10.42abudhabi.ae>
2022-08-18 21:47:55 +0400

    Bonus section revised

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: jalfonso <jalfonso@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 16:41:04 by jalfonso          #+#    #+#             */
-/*   Updated: 2022/08/18 17:10:08 by jalfonso         ###   ########.fr       */
+/*   Updated: 2022/08/18 21:46:24 by jalfonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	void	*temp_pos;
-	
+
 	while (*lst)
 	{
 		temp_pos = (*lst)->next;
@@ -24,3 +24,24 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	}
 	*lst = NULL;
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+//
+// // void del(void *ptr)
+// // {
+// // 	free(ptr);
+// // }
+//
+// int	main(void)
+// {
+// 	t_list *a;
+// 	t_list *b;
+//
+// 	a = ft_lstnew((int *) 24);
+// 	b = ft_lstnew((int *) 42);
+// 	a->next = b;
+// 	printf("%d\n", (int)a->content);
+// 	printf("%d\n", (int)b->content);
+// 	return (0);
+// }
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: jalfonso <jalfonso@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 12:25:07 by jalfonso          #+#    #+#             */
-/*   Updated: 2022/08/18 18:44:01 by jalfonso         ###   ########.fr       */
+/*   Updated: 2022/08/18 21:44:49 by jalfonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,24 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	del(lst->content);
 	free(lst);
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+//
+// // void del(void *ptr)
+// // {
+// // 	free(ptr);
+// // }
+//
+// int	main(void)
+// {
+// 	t_list *a;
+// 	t_list *b;
+//
+// 	a = ft_lstnew((int *) 24);
+// 	b = ft_lstnew((int *) 42);
+// 	a->next = b;
+// 	printf("%d\n", (int)a->content);
+// 	printf("%d\n", (int)b->content);
+// 	return (0);
+// }
